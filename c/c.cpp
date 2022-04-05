3c791bf74a9e33e3ff759a686b8e8235b722413e
TUGUME DANIEL <tugumedaniel2001@gmail.com>
2022-04-05 17:02:36 +0300

    Update c.cpp

--- c/c.cpp
+++ c/c.cpp
@@ -1,1 +1,17 @@
-
+//A program in c
+/** Assignment 2A **
+*** Number c ******/
+#include <stdio.h>
+int main()
+{
+  printf("We sometimes abandon our prize 2 look at others\n");
+  printf("\'Wishing & Hoping\'\nT\nH\nA\nT\t");
+  printf("It could be given to US.\n");
+  printf("When we do have a chance to \"take a peek\"\n");
+  printf("\twe cower and hiss at what might e INSIDE\n");
+  printf("Please\nUNRAVEL THE RIBBON, ");
+  printf("take the \"wrapper\" off that is\n");
+  printf("of \\U\\\nand open the box.");
+  printf("\\U\\ may\n\t\'C THE STARS\'");
+  return 0;
+}
