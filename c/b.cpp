f4c8c0d413f52e254537a5424fcf7abd00034e6c
TUGUME DANIEL <tugumedaniel2001@gmail.com>
2022-04-05 16:51:21 +0300

    Update b.cpp

--- c/b.cpp
+++ c/b.cpp
@@ -1,1 +1,13 @@
-
+//A program in c
+/** Assignment 2A **
+*** Number b ******/
+#include <stdio>
+int main()
+{
+  printf("T\nA\nK\nE\t\'CRITISM\'\n");
+  printf("\"CONSTRUCTIVELY\"\n");
+  printf("NOT \"PERSONALLY\"\n");
+  printf("\tAND SEE IT AS\n");
+  printf("\'AN OPPORTUNITY\' TO IMPROVE YOURSELF\n");
+  return 0;
+}
