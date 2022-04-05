d866af0ec497a837a709b242f660d4be405c9aa5
TUGUME DANIEL <tugumedaniel2001@gmail.com>
2022-04-05 16:41:58 +0300

    Update a.cpp

--- c/a.cpp
+++ c/a.cpp
@@ -1,8 +1,12 @@
 //A program in c
+/** Assignment 2A **
+*** Number a ******/
 #include <stdio.h>
 
 int main()
 {
-  printf("Hello");
+  printf("TYPING IS NO SUBSTITUTE FOR THINKING\n");
+  printf("THE MORE I \'C\'\nTHE LESS I SEE\n");
+  printf("\"C IS QUIRKY AND AN ENORMOUS SUCCESS\"");
   return 0;
 }
