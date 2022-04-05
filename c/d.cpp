28f59fbd9999a0e384f589267ef0187d0aee1302
TUGUME DANIEL <tugumedaniel2001@gmail.com>
2022-04-05 18:18:01 +0300

    Create d.cpp

---
+++ c/d.cpp
@@ -0,0 +1,21 @@
+//A program in c
+/** Assignment 2A **
+*** Number d ******/
+
+#include <stdio.h>
+int main()
+{
+  printf("\tIncreasingly, people seem");
+  printf(" to misinterprete\n");
+  printf("\'complexity\' as \"");
+  printf("sophistication\", which is ");
+  printf("100% baffling\n");
+  printf("\tThe incomprehensible");
+  printf(" should cause\n");
+  printf("\'suspicion\' ratherthan ");
+  printf("\"admiration\".\n");
+  printf("Possibly this trend results from a mistaken belief that\n");
+  printf("using a somewhat mysterious device confers\n");
+  printf("an \\aura\\ of power to the user.\n");
+  return 0;
+}
