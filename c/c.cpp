1887b733b3f60a13bbcf99e83f2e24e1d69c0371
TUGUME DANIEL <tugumedaniel2001@gmail.com>
2022-04-05 18:34:04 +0300

    Update c.cpp

--- c/c.cpp
+++ c/c.cpp
@@ -4,11 +4,15 @@
 #include <stdio.h>
 int main()
 {
-  printf("We sometimes abandon our prize 2 look at others\n");
-  printf("\'Wishing & Hoping\'\nT\nH\nA\nT\t");
+  printf("We sometimes abandon our ");
+  printf("prize 2 look at others\n");
+  printf("\'Wishing & Hoping\'");
+  printf("\nT\nH\nA\nT\t");
   printf("It could be given to US.\n");
-  printf("When we do have a chance to \"take a peek\"\n");
-  printf("\twe cower and hiss at what might e INSIDE\n");
+  printf("When we do have a chance to ");
+  printf("\"take a peek\"\n");
+  printf("\twe cower and hiss at what might ");
+  printf("be INSIDE\n");
   printf("Please\nUNRAVEL THE RIBBON, ");
   printf("take the \"wrapper\" off that is\n");
   printf("of \\U\\\nand open the box.");
